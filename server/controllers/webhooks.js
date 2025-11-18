bc6f2dbe18769ddc245ab5c67b4dfa09e909ce45
vikashshukla123 <vkshukla272003@gmail.com>
2025-11-18 15:38:06 +0530

    corrected user creation

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -22,31 +22,31 @@ export const clerkWebhooks = async (req,res)=>{
                     imageUrl : data.image_url,
                 }
                 await User.create(userData)
-                res.JSON({})
+                res.json({})
                 break;
             }
             case 'user.updated' : {
                  const userData = {
-                    email : data.email_address[0].email_address,
+                    email : data.email_addresses[0].email_address,
                     name : data.first_name + " " + data.last_name,
                     imageUrl : data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id,userData)
-                res.JSON({})
+                res.json({})
                 break;
 
             }
 
             case 'user.deleted' : {
                 await User.findByIdAndDelete(data.id)
-                res.JSON({})
+                res.json({})
                 break;
             }
             default : break
 
         }
     } catch (error){
-        res.JSON({success : false, message : error.message})
+        res.json({success : false, message : error.message})
     }
 
 

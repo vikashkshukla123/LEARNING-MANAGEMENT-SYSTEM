20242d0e484d85622603e5d8fdbcd7a7947e140a
vikashshukla123 <vkshukla272003@gmail.com>
2026-07-19 14:48:15 +0530

    1 MORE

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -9,13 +9,13 @@ export const clerkWebhooks = async (req, res) => {
     const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
     // Verify using raw body
-    wh.verify(req.rawBody, {
-      "svix-id": req.headers["svix-id"],
-      "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"],
-    });
+    const evt = wh.verify(req.body, {
+  "svix-id": req.headers["svix-id"],
+  "svix-timestamp": req.headers["svix-timestamp"],
+  "svix-signature": req.headers["svix-signature"],
+});
 
-    const { data, type } = req.body;
+const { data, type } = evt;
 
     switch (type) {
       case "user.created": {

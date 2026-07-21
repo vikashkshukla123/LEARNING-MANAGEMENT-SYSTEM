f38387bba09bf69348c802784adcc325d4c77fe8
vikashshukla123 <vkshukla272003@gmail.com>
2026-07-21 22:12:55 +0530

    one more

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -2,6 +2,7 @@ import { Webhook } from "svix";
 import User from "../models/User.js";
 import Stripe from "stripe";
 import Purchase from "../models/Purchase.js";
+import Course from "../models/Course.js";
 
 export const clerkWebhooks = async (req, res) => {
   console.log("Clerk webhook called");
